25bc1051b0ae05d2012d49e4afab90d6b0e85e34
Alexandr Peretyatko <keeper_1992@mail.ru>
2023-03-07 11:16:05 +0600

    add postgresql

---
+++ FinBot/config.py
@@ -0,0 +1,5 @@
+from telebot import TeleBot
+
+
+TOKEN = '6162744060:AAE-JWL7CCRptk4MRUvD4EM7fPV8uEfJyhg'
+bot = TeleBot(TOKEN)
--- FinBot/settings.py
+++ FinBot/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'FinBot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
