dc34ad43b9183aa0a73355959dcedfe898a7d76d
Alexandr Peretyatko <keeper_1992@mail.ru>
2023-03-22 10:56:04 +0300

    add api, fix

--- expenses_app/api.py
+++ expenses_app/api.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from django.db.models import Max, F
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
@@ -7,7 +6,7 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from expenses_app.models import Expenses
 from expenses_app.serializers import ExpensesSerializer
-from expenses_app.servise import top_cat, max_ex
+from expenses_app.servise import top_cat
 
 
 class ExpensesViewSet(viewsets.ModelViewSet):
@@ -16,6 +15,7 @@ class ExpensesViewSet(viewsets.ModelViewSet):
 
 
 class ExpensesList(ListModelMixin, CreateModelMixin, GenericAPIView):
+    """Апи для вывода списка трат"""
     serializer_class = ExpensesSerializer
 
     def get_queryset(self):
@@ -34,6 +34,7 @@ class ExpensesList(ListModelMixin, CreateModelMixin, GenericAPIView):
 
 
 class TopCategory(APIView):
+    """Апи для вывода популярных категорий"""
 
     def get(self, request):
         queryset = Expenses.objects.all()
@@ -47,27 +48,27 @@ class TopCategory(APIView):
         if num:
             k = num
         queryset = top_cat(queryset, k)
+
         print(queryset)
         return Response(queryset)
 
 
+class ExpensivePurchases(ListModelMixin, CreateModelMixin, GenericAPIView):
+    """Апи для вывода максимальных трат"""
+    serializer_class = ExpensesSerializer
 
-# class ExpensivePurchases(ListModelMixin, CreateModelMixin, GenericAPIView):
-#     serializer_class = ExpensesSerializer
-#
-#     def get_queryset(self):
-#         queryset = Expenses.objects.annotate(max_money=Max('money')).filter(money=F('max_money')).first()
-#         user_name = self.request.query_params.get('user')  # параметр выбора пользователя
-#         num = self.request.query_params.get('num')  # параметр выбора количества категорий
-#         k = 1
-#         if user_name:
-#             user = User.objects.get(username=user_name)
-#             queryset = queryset.filter(user=user.id)
-#         if num:
-#             k = num
-#         for _ in range(k):
-#             print(Expenses.objects.annotate(max_money=Max('money')).filter(money=F('max_money')).first())
-#         return queryset
-#
-#     def get(self, request):
-#         return self.list(request)
+    def get_queryset(self):
+        queryset = Expenses.objects.all()
+        user_name = self.request.query_params.get('user')  # параметр выбора пользователя
+        num = self.request.query_params.get('num')  # параметр выбора количества трат
+        k = 1
+        if user_name:
+            user = User.objects.get(username=user_name)
+            queryset = queryset.filter(user=user.id)
+        if num:
+            k = int(num)
+        queryset = queryset.order_by('-money')[:k]
+        return queryset
+
+    def get(self, request):
+        return self.list(request)
--- expenses_app/servise.py
+++ expenses_app/servise.py
@@ -1,7 +1,5 @@
 import itertools
 
-from django.db.models import Max, F
-
 
 def top_cat(queryset, k):
     cat_dict = {}
@@ -14,8 +12,3 @@ def top_cat(queryset, k):
     sorted_dict = {k: v for k, v in sorted_tuples}
     print(sorted_dict)
     return dict(itertools.islice(sorted_dict.items(), int(k)))
-
-
-def max_ex(queryset, ex_dict):
-    max_object = queryset.annotate(max_money=Max('money')).filter(money=F('max_money')).first()
-    print(max_object)
